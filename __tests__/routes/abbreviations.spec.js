bde884fefbddf35fb8405a638c165618414667e0
mattia richetto <mattia.richetto@gmail.com>
2017-05-18 21:55:52 -0700

    add abbreviations tests

---
+++ __tests__/routes/abbreviations.spec.js
@@ -0,0 +1,32 @@
+const abbreviations = require('../../routes/abbreviations');
+
+describe('abbreviations', () => {
+  const scenarios = [
+    {
+      input: 'a few seconds',
+      output: 'seconds'
+    },
+    {
+      input: '2 hours',
+      output: '2 hrs'
+    },
+    {
+      input: 'an hour',
+      output: '1 hr'
+    },
+    {
+      input: 'a minute',
+      output: '1 min'
+    },
+    {
+      input: '2 minutes',
+      output: '2 mins'
+    }
+  ];
+
+  scenarios.forEach((scenario) => {
+    test(`"${scenario.input}" => "${scenario.output}"`, () => {
+      expect(abbreviations(scenario.input)).toEqual(scenario.output);
+    });
+  });
+});
